63024492a1a8ea9d35128bfcee043b516e82b73d
Agung Saputra <agungsaputradifh@gmail.com>
2024-04-20 22:21:33 +0700

    nav v0.0.1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Home from './components/Home';
 import Lorem from './components/Lorem';
+import Navbar from './components/Navbar';
 import './components/css/output.css';
 
 function App() {
   return (<>
+    <Navbar/>
     <Home/>
     <Lorem/>
   </>);
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return (<>
+        <nav className="fixed top-0 left-0 right-0 flex justify-center py-10 gap-5 text-2xl">
+            <a href="yt.com">Home</a>
+            <a href="yt.com">Skills</a>
+            <a href="yt.com">Projects</a>
+            <a href="yt.com">About Me</a>
+            <a href="yt.com">Contact</a>
+        </nav>
+    </>)
+}

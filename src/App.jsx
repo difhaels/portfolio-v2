f6b9e9aa38dfeda71e450fd31ea8c81342f18723
Agung Saputra <agungsaputradifh@gmail.com>
2024-04-25 11:27:09 +0700

    added icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from './components/Home';
 import Navbar from './components/Navbar';
+import Skills from './components/Skills';
 import './components/css/output.css';
 
 function App() {
   return (<>
     <Navbar/>
     <Home/>
+    <Skills/>
   </>);
 }
 

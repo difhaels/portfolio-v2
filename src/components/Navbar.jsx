7df8f0da9edbcfb41991016ceaa019186ec6ef44
Agung Saputra <agungsaputradifh@gmail.com>
2024-04-25 10:55:58 +0700

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,9 +31,10 @@ export default function Navbar(){
     }, []);
 
     return (<>
+    <div className="relative z-[99]">
         <motion.path className="fixed top-0 left-0 right-0 h-[10px] bg-[#9370DB] transform origin-top-left" style={{ scaleX, originX: 0}}/>
-        <nav className="fixed top-0 left-0 right-0 flex justify-center pt-5 gap-5 text-[1.3rem] font-bold">
-            <div className={`${navScroll ? "navbar-fixed" : "navbar-unfixed"} nav-scroll  flex justify-center gap-3`}>
+        <nav className="fixed top-0 left-0 right-0 flex justify-center pt-5 gap-5 text-[1.3rem] font-bold ">
+            <div className={`${navScroll ? "navbar-fixed" : "navbar-unfixed"} nav-scroll  flex justify-center gap-3 bg-white bg-opacity-55`}>
                 <a href="yt.com" className="group"><span className="qmark">♔</span><span className="px-1">Home</span><span className="qmark">♕</span></a>
                 <a href="yt.com" className="group"><span className="qmark">♗</span><span className="px-1">Skills</span><span className="qmark">♗</span></a>
                 <a href="yt.com" className="group"><span className="qmark">♖</span><span className="px-1">Projects</span><span className="qmark">♖</span></a>
@@ -41,5 +42,6 @@ export default function Navbar(){
                 <a href="yt.com" className="group"><span className="qmark">♙</span><span className="px-1">Contact</span><span className="qmark">♙</span></a>
             </div>
         </nav>
+    </div>
     </>)
 }

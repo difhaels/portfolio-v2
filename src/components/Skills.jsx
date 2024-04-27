1d250a70a69836f525e9757b47890a67fc78c0ce
Agung Saputra <agungsaputradifh@gmail.com>
2024-04-27 23:52:10 +0700

    new tamplate skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -14,90 +14,140 @@ import canva from './image/svg/canva.svg'
 
 export default function Skills(){
     return (<>
-        <div className='relative mb-[500px]'>
+        <div className='relative mb-[200px]'>
 
-            <div className='w-fit text-center ml-[23%] py-5'>
-                <h1 className='text-xl font-semibold'><span className="text-[#CB1DCD]">♗</span><span className="px-1">Skills</span><span className="text-[#CB1DCD]">♗</span></h1>
-                <h1 className='text-2xl font-bold'>My TechStack and Skills</h1>
-            </div>
-            <div className="relative">
-                <div className='px-8 pb-8 w-1/3 bg-[#EBEBEB] rounded-2xl border-2 shadow-2xl absolute left-[15%]'>
-                <h1 className='text-xl text-center font-bold my-5'>Web Develpment</h1>
-                    <div className='grid grid-cols-3 justify-center items-center gap-4'>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={html} alt="html"/>
-                            </a>
+
+
+            <div className="grid grid-cols-2 gap-10 mx-[15%] items-start">
+
+                <div className="grid gap-10">    
+                    <div className='px-8 pb-8 bg-[#EBEBEB] rounded-2xl border-2 shadow-2xl w-full'>
+                        <h1 className='text-xl text-center font-bold my-5'>Web Develpment</h1>
+                        <div className='grid grid-cols-3 justify-center items-center gap-4'>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={html} alt="html"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={css} alt="css"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={javascript} alt="javascript"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={tailwindcss} alt="tailwindcss"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={bootstrap} alt="bootstrap"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={react} alt="react"/>
+                                </a>
+                            </div>
+                            
                         </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={css} alt="css"/>
-                            </a>
+                    </div>
+                    <div className='px-8 pb-8 bg-[#EBEBEB] rounded-2xl border-2 shadow-2xl w-full'>
+                        <h1 className='text-xl text-center font-bold my-5'>Web Develpment</h1>
+                        <div className='grid grid-cols-3 justify-center items-center gap-4'>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={html} alt="html"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={css} alt="css"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={javascript} alt="javascript"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={tailwindcss} alt="tailwindcss"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={bootstrap} alt="bootstrap"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={react} alt="react"/>
+                                </a>
+                            </div>
+                            
                         </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={javascript} alt="javascript"/>
-                            </a>
-                        </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={tailwindcss} alt="tailwindcss"/>
-                            </a>
-                        </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={bootstrap} alt="bootstrap"/>
-                            </a>
-                        </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={react} alt="react"/>
-                            </a>
-                        </div>
-                        
                     </div>
                 </div>
 
-                <div className='px-8 pb-8 w-1/3 bg-[#EBEBEB] rounded-2xl border-2 shadow-2xl absolute right-[15%] -top-16 z-10'>
-                    <h1 className='text-xl text-center font-bold my-5'>Other</h1>
-                    <div className='grid grid-cols-3 justify-center items-center gap-4'>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={nodejs} alt="nodejs"/>
-                            </a>
-                        </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={php} alt="php"/>
-                            </a>
-                        </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={laravel} alt="laravel"/>
-                            </a>
-                        </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={mysql} alt="mysql"/>
-                            </a>
-                        </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={java} alt="java"/>
-                            </a>
-                        </div>
-                        <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
-                            <a href="yt.com">
-                                <img src={canva} alt="canva"/>
-                            </a>
+                <div className='mt-16 grid gap-5'>
+                    
+                    <div className='w-fit text-center mx-auto'>
+                        <h1 className='text-xl font-semibold'><span className="text-[#CB1DCD]">♗</span><span className="px-1">Skills</span><span className="text-[#CB1DCD]">♗</span></h1>
+                        <h1 className='text-2xl font-bold'>My TechStack and Skills</h1>
+                    </div>
+
+                    <div className='px-8 pb-8 bg-[#EBEBEB] rounded-2xl border-2 shadow-2xl -top-16 z-10 w-full h-fit'>
+                        <h1 className='text-xl text-center font-bold my-5'>Other</h1>
+                        <div className='grid grid-cols-3 justify-center items-center gap-4'>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={nodejs} alt="nodejs"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={php} alt="php"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={laravel} alt="laravel"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={mysql} alt="mysql"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={java} alt="java"/>
+                                </a>
+                            </div>
+                            <div className='border-4 border-[#9370DB] px-5 py-5 rounded-2xl hover:scale-110'>
+                                <a href="yt.com">
+                                    <img src={canva} alt="canva"/>
+                                </a>
+                            </div>
                         </div>
                     </div>
+                    <div className='w-fit bg-[#EBEBEB] rounded-2xl border-2 shadow-2xl mx-auto flex items-center gap-2 px-3 py-2'>
+                        <div className='h-4 w-4 bg-black rounded-full'></div>
+                        <span className='font-semibold'>Would</span>
+                    </div>
                 </div>
+
             </div>
 
             <div>
-                <div className="absolute -bottom-[470px] right-[31%] w-[18%] h-64 border-r-4 border-b-4 border-[#9370DB]"></div>
-                <div className="absolute -bottom-[630px] right-[31%] w-[18%] h-40 border-l-4 border-[#9370DB]"></div>
+                <div className="absolute -bottom-[110px] right-[31%] w-[18%] h-80 border-r-4 border-b-4 border-[#9370DB]"></div>
+                <div className="absolute -bottom-[270px] right-[31%] w-[18%] h-40 border-l-4 border-[#9370DB]"></div>
             </div>
 
         </div>

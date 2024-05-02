708ee7b2ea9f069d196215882532377bdd9ba403
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-02 23:32:49 +0700

    added about file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './components/Projects';
 import Slide1 from './components/projects/Slide1';
 import Slide2 from './components/projects/Slide2';
 import Slide3 from './components/projects/Slide3';
+import About from './components/About';
 
 function App() {
   return (<>
@@ -13,6 +14,7 @@ function App() {
     <Home/>
     <Skills/>
     <Projects slide1={Slide1} slide2={Slide2} slide3={Slide3}/>
+    <About/>
   </>);
 }
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,9 +18,9 @@ import quotebg from './image/png/quotebg.png';
 export default function Projects(props) {
     return (
         <>
-            <div className=' relative'>
-                    <img src={quotebg} alt="quotebg" className='h-[750px] w-full object-cover blur-sm brightness-[.3] -skew-y-[4deg] '/>
-                    <div className="absolute left-0 right-0 top-28">
+            <div className='relative'>
+                    <img src={quotebg} alt="quotebg" className='relative h-[750px] w-full object-cover blur-sm brightness-[.3] -skew-y-[4deg] z-10'/>
+                    <div className="absolute left-0 right-0 top-28 z-20">
                         <Swiper
                             effect={"cards"}
                             grabCursor={true}
@@ -34,6 +34,7 @@ export default function Projects(props) {
                             <SwiperSlide><props.slide3/></SwiperSlide>
                         </Swiper>
                     </div>
+                    <div className="absolute -bottom-[380px] right-0 left-0 mx-auto w-1 h-96 bg-[#9370DB]"></div>
             </div>
         </>
     );

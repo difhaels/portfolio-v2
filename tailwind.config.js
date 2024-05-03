9c74d658b759b75d32cc376308b79dedf09f2ad7
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-03 23:03:23 +0700

    template contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'cyberpunk-purple': '#9370DB',
+      },
+    }
   },
   plugins: [],
 }

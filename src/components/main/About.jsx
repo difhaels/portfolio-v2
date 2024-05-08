a470e8893b5299b72777fb5de7d9b006e5cdc9c5
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-08 09:56:48 +0700

    added line about

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -1,5 +1,8 @@
 export default function About(){
     return(<>
+        <div className="w-[50%] relative">
+            <div className="w-96 h-52 border-t-4 border-l-4 border-[#9370DB] absolute -right-[2px] -top-1"></div>
+        </div>
         <div className="mt-96"> ini adalah About</div>
     </>)
 }

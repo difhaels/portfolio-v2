3bb74b07d4d39d4497642168b385c164dcaaf7c5
Agung Saputra <agungsaputradifh@gmail.com>
2024-05-12 23:27:09 +0700

    added img about

--- src/components/main/About.jsx
+++ src/components/main/About.jsx
@@ -1,15 +1,18 @@
+import agung from '../image/png/agung.png';
+
 export default function About(){
     return(<>
         <div className="relative mt-96">
             <div className="flex justify-center">
                 <div className="w-96 h-52 border-t-4 border-l-4 border-[#9370DB] mr-[380px] -top-1 absolute "></div>
             </div>
-            <div class="flex justify-center">
-                <div class="mt-96">
-                    <h1>text</h1>
+            <div class="flex mt-52 justify-center">
+                <div className='text-center'>
+                    <h1 className='font-semibold text-2xl py-6'>About Me</h1>
+                    <img src={agung} alt="agung saputra" className="rounded-full h-40 w-40 mx-auto border-2" />
                 </div>
-                <div class="mt-96">
-                    <h1>text</h1>
+                <div>
+                    <div></div>
                 </div>
             </div>
         </div>
